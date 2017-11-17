e45148dcef6757be6d29f0754fa958b163040922
cheulong <searcheulong@gmail.com>
2017-11-17 14:52:59 +0700

    final

--- src/main/java/camt/cbsd/dao/StudentDaoDBImpl.java
+++ src/main/java/camt/cbsd/dao/StudentDaoDBImpl.java
@@ -49,7 +49,7 @@ public class StudentDaoDBImpl implements StudentDao {
     }
     @Override
     public List<Student>getStudents(String searchText){
-       // return studentRepository.findByNameIgnoreCaseContaining(searchText);
+      // return studentRepository.findByNameIgnoreCaseContaining(searchText);
         return studentRepository.findByNameIgnoreCaseContainingOrSurnameIgnoreCaseContaining(searchText, searchText);
     }
 }
